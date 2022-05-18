9601157d8bc311557d0ec67c1e18e8c532962d09
Voncay <voncay@yahoo.com>
2022-05-18 04:37:00 +0000

    add database

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+## Get CSV
 YEAR = "2021"
 MONTH = "04"
 
@@ -8,10 +9,26 @@ data = requests.get(remote_url)
 with open(local_file, 'wb')as file:
     file.write(data.content)
 
+
 ## Read CSV
 import pandas
 data = pandas.read_csv("events.csv")
 
-## Get some datas
+
+## Extract some datas
 most_active_user=data.email.mode()[0]
 print("The most active user is {}".format(most_active_user))
+
+
+## Load CSV to Database
+from pathlib import Path
+Path('events.db').touch()
+
+import sqlite3
+conn = sqlite3.connect('events.db')
+c = conn.cursor()
+
+c.execute('''CREATE TABLE events (id,timestamp,email,country,ip,uri,action,tags)''')
+
+data.to_sql('events', conn, if_exists='append', index = False)
+c.execute('''SELECT * FROM events ORDER BY ROWID ASC LIMIT 1''').fetchall()
